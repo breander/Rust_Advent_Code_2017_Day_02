6907dd480d4bcc55b04b65d7abda93947e7d34e4
Brian Davenport <breander@gmail.com>
2018-09-20 19:29:58 -0400

    added input file and ability to read file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut f = File::open("input.txt").expect("File not found!");
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents).expect("Something went wrong!");
+
+    part1(&contents);
+}
+
+fn part1(contents: &String){
+    let row_string = contents.split("\n");
+    let rows: Vec<&str> = row_string.collect();
+
+    for i in &rows {
+        println!("{} ", i);
+    }
 }
