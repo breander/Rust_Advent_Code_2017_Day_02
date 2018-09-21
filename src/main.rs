66ba6741118bca5992c86345b1674e9592630006
Brian Davenport <breander@gmail.com>
2018-09-20 20:38:15 -0400

    implimented part1 and 2.

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,79 @@ fn main() {
     f.read_to_string(&mut contents).expect("Something went wrong!");
 
     part1(&contents);
+    part2(&contents);
 }
 
 fn part1(contents: &String){
     let row_string = contents.split("\n");
     let rows: Vec<&str> = row_string.collect();
 
-    for i in &rows {
-        println!("{} ", i);
+    let mut sum: i32 = 0;
+    for row in &rows {
+        //println!("{} ", i);
+       let number_string = row.split("\t");
+       let numbers: Vec<&str> = number_string.collect();
+       let mut large: i32 = 0;
+       let mut small: i32 = 99999;
+
+       for num in numbers{
+            let current: i32 = num.parse().unwrap();
+            if current > large{
+                large = current;
+            }
+
+            if current < small{
+                small = current;
+            }
+       } 
+
+       sum = sum + (large - small);
     }
+
+    println!("Part1: {}", sum);
+}
+
+fn part2(contents: &String){
+    let row_string = contents.split("\n");
+    let rows: Vec<&str> = row_string.collect();
+
+    let mut sum: i32 = 0;
+    for row in &rows {
+        //println!("{} ", i);
+       let number_string = row.split("\t");
+       //let number_string2 = row.split("\t");
+       let numbers: Vec<&str> = number_string.collect();
+       //let numbers2: Vec<&str> = number_string2.collect();
+       //let mut large: i32 = 0;
+       //let mut small: i32 = 99999;
+
+       for x in 0..numbers.len(){
+            let current: i32 = numbers[x].parse().unwrap();
+            let mut found: bool = false;
+
+            for y in 0..numbers.len() {
+                if x == y{
+                    continue;
+                }
+
+                let current2: i32 = numbers[y].parse().unwrap();
+
+                if current > current2 {
+                    if current % current2 == 0{
+                        sum = sum + (current / current2);
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            if found {
+                break;
+            }
+       } 
+
+       //sum = sum + (large - small);
+    }
+
+    println!("Part2: {}", sum);
 }
